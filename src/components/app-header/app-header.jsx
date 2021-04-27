3ecf8b1a89e5a150f297f510c312e6f80861bb69
spdimmy <spdimmy@gmail.com>
2021-04-27 17:58:25 +0200

    header component styled

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import styles from  './app-header.module.css';
-import { Logo, BurgerIcon, ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 class AppHeader extends React.Component {
   render() {
     return (
-      <header className={styles.header}>
-
+      <header className={`pt-2 pb-2 ${styles.header}`}>
+        <div className={`container ${styles.container}`}>
+          <nav>
+            <ul className={styles.header__menu}>
+              <li className="mr-1">
+                <a href="./" className={`pt-2 pb-2 ${styles.button} ${styles.active}`}>
+                  <BurgerIcon type="primary" />
+                  <span>Конструктор</span>
+                </a>
+              </li>
+              <li>
+                <a href="./" className={`pt-2 pb-2 ${styles.button}`}>
+                  <ListIcon  type="secondary" />
+                  <span>Лента заказов</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <a href="./" className={styles.header__logo}>
+            <Logo />
+          </a>
+          <a href="./" className={`pt-2 pb-2 ${styles.button}`}>
+            <ProfileIcon type="secondary" />
+            <span>Личный кабинет</span>
+          </a>
+        </div>
       </header>
     )
   }
