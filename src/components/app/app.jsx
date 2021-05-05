8b61ee6a0e10f229bfa9fb5d3c3c28a5574abceb
spdimmy <spdimmy@gmail.com>
2021-05-05 17:58:34 +0200

    replace data with fetch query

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import {React, useEffect, useState} from 'react';
 import styles from './app.module.css';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor  from "../burger-constructor/burger-constructor";
-import data from "../../utils/data";
 
 function App() {
+  const url = 'https://norma.nomoreparties.space/api/ingredients';
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(ingredients => setData(ingredients.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className={styles.app}>
       <AppHeader />
